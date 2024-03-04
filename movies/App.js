49a12f708152d6cbce43776f03864f364ab6aaf5
k-oyun <dhdbs001208@naver.com>
2024-03-04 18:11:57 +0900

    update

--- movies/App.js
+++ movies/App.js
@@ -6,7 +6,7 @@ import {Ionicons} from "@expo/vector-icons";
 import {Asset, useAssets} from "expo-asset";
 
 export default function App() {
-  //이전 방식처럼 loading중 특정 작업을 수정하지는 못함 but asset을 preload만 한다면 이 hook을 사용하는 것이 좋음!
+  //이전 방식처럼 loading중 특정 작업을 수행하지는 못함 but asset을 preload만 한다면 이 hook을 사용하는 것이 좋음!
   const [assets] = useAssets([require("./pig.jpeg")]);
   const [loaded] = Font.useFonts(Ionicons);
   //not ready state
