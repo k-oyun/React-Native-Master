a1ef37e2aa610ca746b82548440d841f5ddf4675
k-oyun <dhdbs001208@naver.com>
2024-03-04 18:11:31 +0900

    app loading part three
    이전 방식처럼 loading중 특정 작업을 수행하지는 못함 but asset을 preload만 한다면 이 hook을 사용하는 것이 좋음!

--- movies/App.js
+++ movies/App.js
@@ -3,37 +3,15 @@ import React, {useState} from "react";
 import * as Font from "expo-font";
 import {Text, Image} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
-import {Asset} from "expo-asset";
+import {Asset, useAssets} from "expo-asset";
 
 export default function App() {
-  //apploading은 로딩이 끝날때까지 splash screen을 보여준다
-  const [ready, setReady] = useState(false);
-
-  //로딩이 끝나는 경우
-  const onFinish = () => setReady(true);
-
-  //로딩중인 경우 무엇을 할건지
-  const startLoading = async () => {
-    //Ionicon에 접근할 수 있음
-    await Font.loadAsync(Ionicons.font);
-    //local에 있는 이미지 가져오기
-    await Asset.loadAsync(require("./pig.jpeg"));
-
-    //서버에 있는 이미지 url로 가져오기
-    await Image.prefetch(
-      "https://github.githubassets.com/assets/pull-shark-default-498c279a747d.png"
-    );
-  };
-
+  //이전 방식처럼 loading중 특정 작업을 수정하지는 못함 but asset을 preload만 한다면 이 hook을 사용하는 것이 좋음!
+  const [assets] = useAssets([require("./pig.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons);
   //not ready state
-  if (!ready) {
-    return (
-      <AppLoading
-        onFinish={onFinish}
-        startAsync={startLoading}
-        onError={console.error}
-      />
-    );
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
 
   //ready state
