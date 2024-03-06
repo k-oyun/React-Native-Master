8483afb780b56d9adbe611f11127bf577eea1ea1
k-oyun <dhdbs001208@naver.com>
2024-03-06 17:55:36 +0900

    styled components
    react js와 동일하게 styled components를 사용!

--- movies/navigation/Root.js
+++ movies/navigation/Root.js
@@ -6,7 +6,7 @@ import Stack from "./Stack";
 const Nav = createNativeStackNavigator();
 
 const Root = () => (
-  <Nav.Navigator screenOptions={{presentation: "modal", headerShown: false}}>
+  <Nav.Navigator screenOptions={{presentation: "card", headerShown: false}}>
     <Nav.Screen name="Tabs" component={Tabs} />
     <Nav.Screen name="Stack" component={Stack} />
   </Nav.Navigator>
--- movies/screens/Movies.js
+++ movies/screens/Movies.js
@@ -1,14 +1,23 @@
 import react from "react";
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import {TouchableOpacity} from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
 
 const Movies = ({navigation: {navigate}}) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", {screen: "Three"})}
-    style={{flex: 1, justifyContent: "center", alignItems: "center"}}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", {screen: "Three"})}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
 );
 
 export default Movies;
