8594562bb4df73d65e0d8a658e313d8dc2eaa52f
k-oyun <dhdbs001208@naver.com>
2024-03-06 18:09:48 +0900

    ThemeProvider

--- movies/App.js
+++ movies/App.js
@@ -1,13 +1,15 @@
 import AppLoading from "expo-app-loading";
 import React, {useState} from "react";
 import * as Font from "expo-font";
-import {Text, Image} from "react-native";
+import {Text, Image, useColorScheme} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
 import {Asset} from "expo-asset";
 import {NavigationContainer} from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stack from "./navigation/Stack";
 import Root from "./navigation/Root";
+import {ThemeProvider} from "styled-components/native";
+import {darkTheme, lightTheme} from "./styled";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -24,6 +26,8 @@ export default function App() {
     await Promise.all([...fonts]);
   };
 
+  const isDark = useColorScheme() === "dark";
+
   //not ready state
   if (!ready) {
     return (
@@ -37,8 +41,10 @@ export default function App() {
 
   //ready state
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
